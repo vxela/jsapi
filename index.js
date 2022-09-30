4d5f3efa817a7e9681e7551b89134e5a2ebffc29
413x1 <muhammad.sholeh@sirclo.com>
2022-10-01 04:54:01 +0700

    hello world

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express"
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+
+    res.send('hello world')
+
+})
+
+app.listen(port, () => {
+
+    console.log(`server runing at port ${port}`)
+
+})
