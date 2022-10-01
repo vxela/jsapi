4200f3aa05f7db7698a122136aad3b611c5ca2b5
413x1 <muhammad.sholeh@sirclo.com>
2022-10-01 20:36:25 +0700

    add example for post json and request id

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import express from "express"
 import { app as appConfig } from "./config/general.js"
 
 const app = express()
+app.use(express.json());
 
 app.get('/', (req, res) => {
 
     res.send('hello world')
 
+})
+
+app.get('/:random_number', (req, res) => {
+    res.send(req.params)
+})
+
+app.post('/', (req, res) => {
+    const data = req.body
+    res.send(data)
 })
 
 app.listen(appConfig, () => {
